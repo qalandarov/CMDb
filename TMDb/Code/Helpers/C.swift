f83516685677fa422ccaf7d88bc75a5afa0c48cb
Islam <qalandarov@me.com>
2017-08-31 23:35:04 +0400

    Changed APIKey to be private

--- TMDb/Code/Helpers/C.swift
+++ TMDb/Code/Helpers/C.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 struct C {
-    static let APIKey = "2696829a81b1b5827d515ff121700838"
+    private static let APIKey = "2696829a81b1b5827d515ff121700838"
     static let BaseURLString = "http://api.themoviedb.org/3/search/movie?api_key=\(APIKey)&query=batman"
 }
