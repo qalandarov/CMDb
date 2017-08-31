34bf99b96a18e7a2b4d6044c14d93578bddf1dba
Islam <qalandarov@me.com>
2017-09-01 01:31:34 +0400

    Added MovieGenre

---
+++ TMDb/Code/Core/Models/MovieGenre.swift
@@ -0,0 +1,32 @@
+//
+//  MovieGenre.swift
+//  TMDb
+//
+//  Created by Islam Qalandarov on 9/1/17.
+//  Copyright © 2017 Qalandarov. All rights reserved.
+//
+
+import Foundation
+
+public enum MovieGenre: Int, Decodable {
+    case action = 28
+    case adventure = 12
+    case animation = 16
+    case comedy = 35
+    case crime = 80
+    case documentary = 99
+    case drama = 18
+    case family = 10751
+    case fantasy = 14
+    case foreign = 10769
+    case history = 36
+    case horror = 27
+    case music = 10402
+    case mystery = 9648
+    case romance = 10749
+    case scienceFiction = 878
+    case tvMovie = 10770
+    case thriller = 53
+    case war = 10752
+    case western = 37
+}
