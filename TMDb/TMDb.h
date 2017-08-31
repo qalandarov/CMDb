759f42ac8b3032b2524b2354507e6ae0b5b52ff4
Islam <qalandarov@me.com>
2017-08-31 22:20:20 +0400

    Created TMDb framework

---
+++ TMDb/TMDb.h
@@ -0,0 +1,19 @@
+//
+//  TMDb.h
+//  TMDb
+//
+//  Created by Islam Qalandarov on 8/31/17.
+//  Copyright © 2017 Qalandarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMDb.
+FOUNDATION_EXPORT double TMDbVersionNumber;
+
+//! Project version string for TMDb.
+FOUNDATION_EXPORT const unsigned char TMDbVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDb/PublicHeader.h>
+
+
