47de69f2456f6e6807ff6787928f6be1cbf458ac
Islam <qalandarov@me.com>
2017-09-01 01:30:13 +0400

    Changed url to https

--- TMDb/Code/Helpers/C.swift
+++ TMDb/Code/Helpers/C.swift
@@ -10,5 +10,5 @@ import Foundation
 
 struct C {
     private static let APIKey = "2696829a81b1b5827d515ff121700838"
-    static let BaseURLString = "http://api.themoviedb.org/3/search/movie?api_key=\(APIKey)&query=batman"
+    static let BaseURLString = "https://api.themoviedb.org/3/search/movie?api_key=\(APIKey)&query=batman"
 }
