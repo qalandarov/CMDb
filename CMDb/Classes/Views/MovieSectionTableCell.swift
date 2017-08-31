1b2a9dbd023535322293f87c95953033d7094f12
Islam <qalandarov@me.com>
2017-08-31 20:14:23 +0400

    Added Table vc and a cell for the Home page

---
+++ CMDb/Classes/Views/MovieSectionTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSectionTableCell.swift
+//  CMDb
+//
+//  Created by Islam Qalandarov on 8/31/17.
+//  Copyright © 2017 Qalandarov. All rights reserved.
+//
+
+import UIKit
+
+class MovieSectionTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
