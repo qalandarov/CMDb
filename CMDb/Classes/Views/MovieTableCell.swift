de148140b421c945d8d57726a764671a908cd018
Islam <kalandarov.islom@gmail.com>
2017-10-04 06:30:28 +0400

    Created MovieTableCell class + connected from storyboard

---
+++ CMDb/Classes/Views/MovieTableCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTableCell.swift
+//  CMDb
+//
+//  Created by Islam Qalandarov on 10/4/17.
+//  Copyright © 2017 Qalandarov. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableCell: UITableViewCell {
+    
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var yearLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    
+}
