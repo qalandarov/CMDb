0241531bdad1535f43575e098c81017d17fa9249
Islam <kalandarov.islom@gmail.com>
2017-10-17 23:13:53 +0400

    Created AsyncOperation - Operation subclass to work with async calls (good for networking)

---
+++ CMDb/Classes/Models/AsyncOperation.swift
@@ -0,0 +1,68 @@
+//
+//  AsyncOperation.swift
+//  CMDb
+//
+//  Created by Islam Qalandarov on 10/17/17.
+//  Copyright © 2017 Qalandarov. All rights reserved.
+//
+//  Credits: https://stackoverflow.com/questions/36675986
+
+import Foundation
+
+class AsyncOperation: Operation {
+    enum State: String {
+        case ready, executing, finished
+        
+        fileprivate var keyPath: String {
+            return "is" + rawValue.capitalized
+        }
+    }
+    
+    var state: State = .ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
+        }
+        didSet {
+            didChangeValue(forKey: oldValue.keyPath)
+            didChangeValue(forKey: state.keyPath)
+        }
+    }
+}
+
+extension AsyncOperation {
+    override func start() {
+        if isCancelled {
+            state = .finished
+            return
+        }
+        
+        state = .executing
+    }
+    
+    override func cancel() {
+        state = .finished
+    }
+}
+
+extension AsyncOperation {
+    override var isReady: Bool {
+        return super.isReady && state == .ready
+    }
+    
+    override var isExecuting: Bool {
+        return state == .executing
+    }
+    
+    override var isFinished: Bool {
+        return state == .finished
+    }
+    
+    override var isConcurrent: Bool {
+        return true
+    }
+    
+    override var isAsynchronous: Bool {
+        return true
+    }
+}
