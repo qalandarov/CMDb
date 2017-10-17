17c53a4f05ce72024b88d72646908982a7e594bd
Islam <kalandarov.islom@gmail.com>
2017-10-17 23:29:46 +0400

    Created Movie Section downloader Operation

---
+++ CMDb/Classes/Models/MovieSectionDownloadOperation.swift
@@ -0,0 +1,40 @@
+//
+//  MovieSectionDownloadOperation.swift
+//  CMDb
+//
+//  Created by Islam Qalandarov on 10/17/17.
+//  Copyright © 2017 Qalandarov. All rights reserved.
+//
+
+import Foundation
+import TMDb
+
+class MovieSectionDownloadOperation: AsyncOperation {
+    private lazy var network = NetworkEngine()
+    private var result: Result<Search<Movie>>?
+    
+    var section: MovieSectionType
+    
+    required init(section: MovieSectionType) {
+        self.section = section
+    }
+    
+    override func start() {
+        super.start()
+        
+        network.movies(type: section) { [weak self] result in
+            self?.result = result
+            self?.state = .finished
+        }
+    }
+}
+
+extension MovieSectionDownloadOperation {
+    var movies: [Movie]? {
+        return result?.value?.results
+    }
+    
+    var error: TMDb.Error? {
+        return result?.error
+    }
+}
