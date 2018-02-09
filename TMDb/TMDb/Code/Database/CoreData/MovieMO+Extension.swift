bbc9d3147c05aaec394f14a89a2f9b5d946ab622
Islam <kalandarov.islom@gmail.com>
2018-02-09 11:38:38 +0400

    Added SearchMO and MovieMO helper extensions

---
+++ TMDb/TMDb/Code/Database/CoreData/MovieMO+Extension.swift
@@ -0,0 +1,49 @@
+//
+//  MovieMO+Extension.swift
+//  TMDb
+//
+//  Created by Islam Qalandarov on 2/6/18.
+//  Copyright © 2018 Qalandarov. All rights reserved.
+//
+
+import CoreData
+
+extension MovieMO {
+    
+    @discardableResult
+    static func insertOrUpdate(_ movie: Movie, in context: NSManagedObjectContext) -> MovieMO {
+        if let movieObject = MovieMO.existing(with: movie.id, in: context) {
+            movieObject.modify(with: movie, in: context)
+            return movieObject
+        }
+        
+        return MovieMO(with: movie, in: context)
+    }
+    
+    private static func existing(with id: Int, in context: NSManagedObjectContext) -> MovieMO? {
+        let predicate = NSPredicate(format: "id == %@", id)
+        let movies = MovieMO.fetch(predicate: predicate, from: context)
+        return movies?.first
+    }
+    
+    private convenience init(with movie: Movie, in context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.id = Int64(movie.id)
+        self.modify(with: movie, in: context)
+    }
+    
+    private func modify(with movie: Movie, in context: NSManagedObjectContext) {
+        
+        title           = movie.title
+        originalTitle   = movie.originalTitle
+        overview        = movie.overview
+        backdropPath    = movie.backdropPath
+        posterPath      = movie.posterPath
+        releaseDate     = movie.releaseDate
+        isVideo         = movie.isVideo
+        popularity      = Float(movie.popularity)
+        voteAvg         = Float(movie.voteAvg)
+        voteCount       = Int16(movie.voteCount)
+    }
+    
+}
---
+++ TMDb/TMDb/Code/Database/CoreData/SearchMO+Extension.swift
@@ -0,0 +1,48 @@
+//
+//  SearchMO+Extension.swift
+//  TMDb
+//
+//  Created by Islam Qalandarov on 2/8/18.
+//  Copyright © 2018 Qalandarov. All rights reserved.
+//
+
+import CoreData
+
+extension SearchMO {
+    
+    @discardableResult
+    static func insertOrUpdate(_ search: Search<Movie>, for query: String, in context: NSManagedObjectContext) -> SearchMO {
+        if let searchObject = SearchMO.existing(with: query, in: context) {
+            searchObject.modify(with: search, in: context)
+            return searchObject
+        }
+        
+        return SearchMO(with: search, for: query, in: context)
+    }
+    
+    private static func existing(with query: String, in context: NSManagedObjectContext) -> SearchMO? {
+        let predicate = NSPredicate(format: "query == [c] %@", query)
+        let searches = SearchMO.fetch(predicate: predicate, from: context)
+        return searches?.first
+    }
+    
+    private convenience init(with search: Search<Movie>, for query: String, in context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.query = query
+        self.modify(with: search, in: context)
+    }
+    
+    private func modify(with search: Search<Movie>, in context: NSManagedObjectContext) {
+        updatedAt = Date()
+        
+        page = Int64(search.page)
+        totalPages = Int64(search.totalPages)
+        totalResults = Int64(search.totalResults)
+        
+        for movie in search.results {
+            let movieObject = MovieMO.insertOrUpdate(movie, in: context)
+            movieObject.search = self
+        }
+    }
+    
+}
