fd22ec46e554b56ca10815aa03b00b408ca2a511
Islam <kalandarov.islom@gmail.com>
2018-02-09 11:00:03 +0400

    Improvements on CoreDataStack

--- TMDb/TMDb/Code/Database/CoreData/CoreDataStack.swift
+++ TMDb/TMDb/Code/Database/CoreData/CoreDataStack.swift
@@ -8,51 +8,61 @@
 
 import CoreData
 
+extension NSManagedObjectContext {
+    func saveIfNeeded() {
+        guard hasChanges else { return }
+        
+        do {
+            try save()
+        } catch {
+            assertionFailure("Couldn't save the context")
+        }
+    }
+}
+
 class CoreDataStack {
+    
+    // Built from the mainContext for temporary works
+    func tempWorkerContext() -> NSManagedObjectContext {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.parent = mainContext
+        return context
+    }
+    
+    // Built from the bgContext to be used in the UI layer
+    lazy var mainContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.parent = bgContext
+        return context
+    }()
+    
+    // The only context that writes to the PSC (so if the writing takes too long the UI doesn't lag)
+    lazy var bgContext: NSManagedObjectContext = {
+        return persistentContainer.newBackgroundContext()
+    }()
     
     // MARK: - Core Data stack
     
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-        let container = NSPersistentContainer(name: "TMDb")
+    private lazy var persistentContainer: NSPersistentContainer = {
+        // Getting the bundle as it's a Framework
+        let bundle = Bundle(for: type(of: self))
+        let dataModelName = "TMDb"
+        
+        guard
+            let modelURL = bundle.url(forResource: dataModelName, withExtension:"momd"),
+            let mom = NSManagedObjectModel(contentsOf: modelURL)
+            else {
+                fatalError("Unable to initialize the model")
+        }
+        
+        let container = NSPersistentContainer(name: dataModelName, managedObjectModel: mom)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
         return container
     }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
     
 }
